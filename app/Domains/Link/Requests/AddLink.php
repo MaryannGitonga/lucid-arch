1749704653814d457cbb7dd2fdc90374bd0208f9
Maryann Gitonga <34104277+MaryannGitonga@users.noreply.github.com>
2021-04-12 19:40:19 +0300

    Create an app with lucid architecture

---
+++ app/Domains/Link/Requests/AddLink.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Domains\Link\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+
+class AddLink extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => ['required', 'max:255'],
+            'url' => ['required', 'url', 'max:255'],
+            'description' => ['required', 'max:255'],
+        ];
+    }
+}
---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Lucid\Units\Controller;
+use App\Features\AddLinkFeature;
+
+class LinkController extends Controller
+{
+    public function add()
+    {
+        return $this->serve(AddLinkFeature::class);
+    }
+}
